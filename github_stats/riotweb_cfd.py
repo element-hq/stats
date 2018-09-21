d0ed7f5d4817b59870e009a2d9d286a4c1c8b037
Tom Lant <lampholder@gmail.com>
2018-09-21 14:53:17 +0000

    Bringing cfd generator into version control

---
+++ github_stats/riotweb_cfd.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""Stats script to generate the number of opened/closed issues by priority for every
+day."""
+import sys
+sys.dont_write_bytecode = True
+
+from collections import defaultdict
+from datetime import date, timedelta, datetime
+
+import MySQLdb
+
+from github_stats import ProjectStats, Helper, CONFIG
+
+TABLE_NAME = 'riotweb_cfd_columns'
+SCHEMA = """
+CREATE TABLE IF NOT EXISTS
+%s (
+    date DATE NOT NULL,
+    col VARCHAR(255) NOT NULL,
+    cardcount INT NOT NULL
+);
+""" % TABLE_NAME
+
+# Connect to and setup db:
+db = MySQLdb.connect(host='localhost',
+                     user='businessmetrics',
+                     passwd=CONFIG.MYSQL_PASSWORD,
+                     db='businessmetrics',
+                     port=3306)
+Helper.create_table(db, SCHEMA)
+
+# Pull stats
+counts = ProjectStats.get_column_counts(CONFIG.GITHUB_TOKEN,
+                                        'vector-im/riot-web/projects/11',
+                                        archived_state={'Done': 'all'})
+
+# Persist stats
+cursor = db.cursor()
+today = date.today()
+clear_today = """
+DELETE FROM riotweb_cfd_columns
+WHERE `date` = %s
+"""
+cursor.execute(clear_today, (today, ))
+
+record_counts = """
+INSERT INTO riotweb_cfd_columns
+(date, col, cardcount)
+VALUE (%s, %s, %s)
+"""
+for column, count in counts.items():
+    cursor.execute(record_counts, (today, column, count))
+db.commit()

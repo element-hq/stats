2772fd8de8aa8a0d8ff6b793731475f22b4b0a98
Tom Lant <lampholder@gmail.com>
2018-09-21 15:14:09 +0000

    bringing triaged issue count into version control

---
+++ github_stats/riotweb_issues_triaged.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""Stats script for tracking the number of untriaged issues"""
+from datetime import date, timedelta, datetime
+from collections import defaultdict
+
+import MySQLdb
+
+from github_stats import GithubStats, Helper, CONFIG
+
+TABLE_NAME = 'riotweb_untriaged_count'
+SCHEMA = """
+CREATE TABLE IF NOT EXISTS
+%s (
+    date DATE NOT NULL,
+    total INT NOT NULL
+);
+""" % TABLE_NAME
+
+# Connect to and setup db:
+db = MySQLdb.connect(host='localhost',
+                     user='businessmetrics',
+                     passwd=CONFIG.MYSQL_PASSWORD,
+                     db='businessmetrics',
+                     port=3306)
+Helper.create_table(db, SCHEMA)
+
+stats = GithubStats()
+
+date_format = '%Y-%m-%d'
+today = date.today()
+
+# Thanks for only returning 1000 results in your paginated result set, github.
+untriaged = stats.small_query(query='repo:vector-im/riot-web is:open is:issue no:label')
+untriaged_count = len(list(untriaged))
+
+# Persist to db:
+cursor = db.cursor()
+delete_entries = """
+DELETE FROM riotweb_untriaged_count
+WHERE `date` = %s
+"""
+cursor.execute(delete_entries, (today, ))
+
+record_untriaged_issue_count = """
+INSERT INTO riotweb_untriaged_count
+(date, total)
+VALUES (%s, %s)
+""" 
+cursor.execute(record_untriaged_issue_count, (today, untriaged_count))
+db.commit()

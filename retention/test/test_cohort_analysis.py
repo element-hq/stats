08448bd48712b4a6ea7e5f5189775664a94b6632
Neil Johnson <neil@matrix.org>
2021-05-26 11:05:48 +0100

    respond to style code review comments

--- retention/test/test_cohort_analysis.py
+++ retention/test/test_cohort_analysis.py
@@ -43,7 +43,9 @@ def configure_test_db():
 
         # create tables
         if conn is not None:
-            # TODO Why do I need this?
+            # It is necessary to overide the readonly flag for tests, in order
+            # to set up the database test conditions. The production script
+            # does not require write access.
             conn.set_session(readonly=False)
             create_table(conn, sql_create_users)
             create_table(conn, sql_create_user_daily_visits_table)
@@ -99,12 +101,21 @@ class TestCohortAnalysis(unittest.TestCase):
         configure_test_db()
 
     def test_ts_to_str(self):
+        """Test to ensure converting from unix epoch time (ms) to date "%Y-%m-%d"
+        works as expected.
+        """
         self.assertEqual(cohort_analysis.ts_to_str(1609459200000), "2021-01-01")
 
     def test_str_to_ts(self):
+        """Test to ensure converting from date "%Y-%m-%d" to unix epoch time (ms)
+        works as expected.
+        """
         self.assertEqual(cohort_analysis.str_to_ts("2021-01-01"), 1609459200000)
 
     def test_get_new_users(self):
+        """Test to ensure that the get_new_users method correctly returns new users
+        and respects the timing boundaries.
+        """
         date_to_test = 16207776000000
         TWENTY_FOUR_HOURS = 86400000
 
